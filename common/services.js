245141fa75890e3f8d020688266df0ee52a98e26
Guo_rf <601082022@qq.com>
2016-04-16 16:04:02 +0800

    大功告成！

--- common/services.js
+++ common/services.js
@@ -2,6 +2,32 @@
 app.factory('Tree',function(){
     //类的增强，被过滤器依赖吧
     var self = this;
+    var C = {
+        letChildChange: function(charger,childName){
+            var fatherValue = charger.$ifChoosen
+            if(childName == undefined) childName = 'items'
+            if(charger.$hasChildren() == true){
+                charger[childName].map(function(item){
+                    item.$ifChoosen = fatherValue
+                    C.letChildChange(item,childName)
+                })
+            }
+        },
+        followMyFather: function(father,childName){
+            if(father == undefined) return
+            var _cache_NoChoosen = 0
+            father[childName].map(function(item,index){
+                if(!item.$ifChoosen) _cache_NoChoosen++
+                if(index == father[childName].length-1){
+                    if( _cache_NoChoosen<1){
+                        father.$ifChoosen = true
+                    }else{
+                        father.$ifChoosen = false
+                    }
+                }
+            })
+        }
+    }
     var enhanceItem = function (charger,childName) {
         //console.log(charger,'check if the inherit is done expectlly')
         charger.$hasChildren = function (){
@@ -16,12 +42,18 @@ app.factory('Tree',function(){
                 charger.$ifFolded = true;
             }
         /*ng-click的时候触发是否折叠这个tree*/
-        //todo: 考察這個方法是否有寫錯的嫌疑
         charger.$changeFolded = function (){
             console.log('has changed')
             charger.$ifFolded = !charger.$ifFolded
         }
 
+
+        /* 一旦检测到变换就立刻让执行这两个 */
+        charger.$getChange = function(father){
+            C.followMyFather(father,childName)
+            C.letChildChange (charger,childName)
+        }
+        charger.$choosen = false
         }
      this.followSelf = function (input,childName){
          if(childName == undefined){
@@ -31,9 +63,8 @@ app.factory('Tree',function(){
             input.map(function(item){
                 //強化的是自己
                 enhanceItem(item,childName)
-                //靠的是enhanceitem 的 return，還是代碼自己能找到兒子呢
+                //强化完后可以这样使用这些新的方法来判断是否有儿子了，有立刻调用自身一次
                 if(item.$hasChildren() == true){
-                    //console.log('has child','my id is' + item.id)
                     self.followSelf(item[childName],childName)
                 }
             })
