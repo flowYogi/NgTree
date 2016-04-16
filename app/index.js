a6483b4163a6a4461b1752fae041f0c355d2970f
Guo_rf <601082022@qq.com>
2016-04-16 23:42:22 +0800

    卡住了！

--- app/index.js
+++ app/index.js
@@ -1,7 +1,67 @@
 /*created by Guo_rf*/
 
-var app = angular.module('show',[]);
-app.controller('tree-home',function($scope){
-    var path = ''
+var app = angular.module('show',['ui.tree']);
+app.controller('tree-home',function($scope,Tree){
+    $scope.vm = {
+        path: '../app/tmp/tree-tmp.html'
+    }
+    $scope.data = [
+        {
+            "id": 11,
+            "title": "node1.1",
+            "nodes": []
+        },
+        {
+            "id": 111,
+            "title": "node1.1.1",
+            "nodes": []
+        },
+        {
+            "id": 1,
+            "title": "node1",
+            "nodes": [
+                {
+                    "id": 2,
+                    "title": "node2",
+                    "nodrop": true,
+                    "nodes": [
+                        {
+                            "id": 22,
+                            "title": "node2.2",
+                            "nodes": []
+                        },
+                        {
+                            "id": 21,
+                            "title": "node2.1",
+                            "nodes": []
+                        }
+                    ]
+                },
+                {
+                    "id": 12,
+                    "title": "node1.2",
+                    "nodes": []
+                }
+            ]
+        },
+        {
+            "id": 31,
+            "title": "node3.1",
+            "nodes": [
+                {
+                    "id": 310,
+                    "title": "node3.1.1",
+                    "nodes": []
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "title": "node3",
+            "nodes": []
+        }
+    ]
+    Tree.enhanceItem($scope.data,'nodes')
+
 })
 
--- common/services.js
+++ common/services.js
@@ -1,5 +1,49 @@
 
 app.factory('Tree',function(){
+    var self = this
+    var C = {
+        checkItsChild: function(items,childName){
+            console.log(items)
+            if(items && items[childName].length >0){
+                return true
+            }
+            return false
+        }
+    }
+    var enhanceItem = function(charger,childName){
+        //这里的查询有孩子否的方法已经不是用来展示了，而是用来折叠了
+        charger.$hasChild = function () {
+            return C.checkItsChild(charger,childName)
+        }
+        //如果为false ，表示为展开状态
+        charger.$ifFolded = false
 
+        charger.$changeFolded = function(){
+            charger.ifFolded = !charger.ifFolded
+        }
+        charger.$deleteItself = function () {
+            //未验证过
+            delete charger
+        }
+    }
+
+    this.init = function(items,childName){
+        if(!childName) childName = 'nodes'
+        //首次使用没办法用到C了，毕竟都是对象啊
+        //if(items && items.length >0){
+        //    items.map(function (item) {
+        //        enhanceItem(item,childName)
+        //    })
+        //    if(items.$hasChild()){
+        //        self.init(items[childName])
+        //    }
+        //}
+
+
+        if(items && item.length >0)
+    }
+    return {
+        enhanceItem: this.init
+    }
 }
 )
