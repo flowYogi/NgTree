0c5f81f6076773771649433cb21076d53d7a4657
Guo_rf <601082022@qq.com>
2016-04-16 21:21:42 +0800

    一个新的任务：写一个ui-tree，现在初始化好了

--- app/index.js
+++ app/index.js
@@ -2,62 +2,6 @@
 
 var app = angular.module('show',[]);
 app.controller('tree-home',function($scope){
-    $scope.data = [
-        {id: 1, title: 'orgin1', content: 'orgin1 text', items: [
-            {
-                id: 11,
-                title: 'son1',
-                content: 'son1 text',
-                items: [{
-                    id: 111,
-                    title: 'ss1',
-                    content: 'ss1 text'
-                },{
-                    id: 112,
-                    title: 'ss2',
-                    content: 'ss2 text'
-                }]
-            },
-            {
-                id: 12,
-                title: 'son1',
-                content: 'son1 text',
-                items: [
-                    {
-                        id: 121,
-                        title: 'ss1',
-                        content: 'ss1 text'
-                    },{
-                        id: 122,
-                        title: 'ss2',
-                        content: 'ss2 text'
-                    },
-                    {
-                        id: 122,
-                        title: 'ss2',
-                        content: 'ss2 text'
-                    }
-                ]
-            }
-        ]},
-        {
-            id: 2, title: 'orgin2', content: 'orgin2 text', items: [
-            {
-                id: 21,
-                title: 'son1',
-                content: 'son1 text',
-                items: [{
-                    id: 211,
-                    title: 'ss1',
-                    content: 'ss1 text'
-                }, {
-                    id: 212,
-                    title: 'ss2',
-                    content: 'ss2 text'
-                }]
-
-            }]
-        }
-    ]
+    var path = ''
 })
 
